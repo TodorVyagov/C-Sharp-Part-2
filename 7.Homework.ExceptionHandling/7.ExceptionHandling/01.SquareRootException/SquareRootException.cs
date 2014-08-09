a9829d51e86c005e7d40504738356a4a643df24a
TodorVyagov <rambo007@abv.bg>
2014-08-09 23:23:35 +0300

    Exception handling homework.

---
+++ 7.Homework.ExceptionHandling/7.ExceptionHandling/01.SquareRootException/SquareRootException.cs
@@ -0,0 +1,36 @@
+﻿//Write a program that reads an integer number and calculates and prints its square root. If the number is invalid or negative, print "Invalid number". In all cases finally print "Good bye". Use try-catch-finally.
+using System;
+
+namespace SquareRootException
+{
+    class SquareRootException
+    {
+        static void Main()
+        {
+            Console.Write("This program will calculate the square root of given integer number and if it is invalid or negative will give message.\nEnter number = ");
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentException();
+                }
+                double num = Math.Sqrt(number);
+                Console.WriteLine("The square root of your number is {0}", num);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number! Number must be >= 0!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number! You did not enter correct number!");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye!");
+            }
+                
+        }
+    }
+}
---
+++ 7.Homework.ExceptionHandling/7.ExceptionHandling/02.ReadNumberInGivenRange/ReadNumberInGivenRange.cs
@@ -0,0 +1,41 @@
+﻿//Write a method ReadNumber(int start, int end) that enters an integer number in given range [start…end]. 
+//If an invalid number or non-number text is entered, the method should throw an exception. 
+//Using this method write a program that enters 10 numbers:
+//a1, a2, … a10, such that 1 < a1 < … < a10 < 100
+using System;
+
+namespace ReadNumberInGivenRange
+{
+    class ReadNumberInGivenRange
+    {
+        static void Main()
+        {
+            Console.WriteLine("This program will read 10 numbers in the range [1; 100] and will check if they are correct.\nEnter numbers below:");
+            int[] arrayOfNumbers = new int[10];
+            for (int index = 0; index < arrayOfNumbers.Length; index++)
+            {
+                Console.Write("arr[{0}] = ", index);
+                arrayOfNumbers[index] = ReadNumber(1, 100);
+            }
+            
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            //Console.WriteLine("Enter number in the range [{0}; {1}]: ", start, end);
+            string numToString = Console.ReadLine();
+            int number;
+
+            if (!int.TryParse(numToString, out number))
+            {
+                throw new FormatException();
+            }
+
+            if (number < start || number > end)
+            {
+                throw new ArgumentException("Invalid number! Value is not in the desired range!");
+            }
+            return number;
+        }
+    }
+}
